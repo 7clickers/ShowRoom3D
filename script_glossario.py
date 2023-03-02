87d8308622091665f0cdaf06b7c242a7fd4bd644
mirkostella <mirko.stella@studenti.unipd.it>
2023-03-03 00:37:41 +0100

    #117 aggiunti file per i plurali ma da sistemare script per il riconoscimento

--- script_glossario.py
+++ script_glossario.py
@@ -1,15 +1,16 @@
 import re
 
 # indicare il path del file target
-pathTarget='./latex/interni/verbali/verbale_interno_01-02-23/res/sections/02_content.tex'
-fileTarget='02_content.tex'
-listaNomiDocumenti=["Piano di Progetto","Norme di Progetto","Piano di Qualifica","Glossario"]
+pathTarget='./prova.tex'
+fileTarget=''
+listaNomiDocumenti=["Piano di Progetto","Norme di Progetto","Piano di Qualifica","Glossario","Lettera di Presentazione",
+"Verbale","Studio di Fattibilitá","Diario di Bordo"]
 
 with open(pathTarget,'r') as file:
     fileTarget=file.read()
 
 # pulizia file:rimuove tutti i comandi tex inseriti da questo script nel documento target
-fileTarget=fileTarget.replace('\\textsuperscript{g}',"")
+fileTarget=fileTarget.replace('\\textsubscript{g}',"")
 
 for d in listaNomiDocumenti:
     listaMatchesDocumenti=re.findall("\\\\textit\{{1}"+d+"\}{1}",fileTarget,re.IGNORECASE)
@@ -29,6 +30,7 @@ for l in lett:
         contenuto=contenuto+file.read()
 # POST:contenuto contiene le sezioni riguardanti i termini del glossario
 
+
 sottoSezioni = re.findall("subsection\{{1}[a-zA-Z]*\}{1}%parola",contenuto)
 # sottoSezioni é una lista contenente tutti i tag subsection riguardanti i termini del glossario
 
@@ -38,6 +40,15 @@ while sottoSezioni:
     listaTermini.insert(0,parola.group(0))
 # POST:listaTermini é una lista che contiene tutti i termini del glossario
 
+# legge i file dei plurali e per ciascuna riga inserisce il termine all'interno della lista termini
+for l in lett:
+    with open('./latex/esterni/doc_esterna/glossario/res/sections/alphabet_plurals/'
+    +l+'/'+l+'.tex','r') as file:
+        for riga in file:
+            riga = riga.rstrip()
+            listaTermini.insert(0,riga)
+# POST:la lista dei termini contiene anche i plurali
+print(listaTermini)
 setIS=set()
 for t in listaTermini:
     listaMatches=re.findall(t,fileTarget,re.IGNORECASE)
@@ -48,8 +59,8 @@ for t in listaTermini:
 
 # per ogni termine
 for tIS in setIS:
-    tPedice=tIS+'\\textsuperscript{g}'
-    fileTarget=fileTarget.replace(tIS,tIS+'\\textsuperscript{g}')
+    tPedice=tIS+'\\textsubscript{g}'
+    fileTarget=fileTarget.replace(tIS,tIS+'\\textsubscript{g}')
 # POST:fileTarget contiene i pedici sui termini desiderati
 
 # SCRITTURA DEI NOMI DEI DOCUMENTI NEL FORMATO CORRETTO ========================
