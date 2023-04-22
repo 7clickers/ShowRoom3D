3fcaa2f8a160139d806a70e437f62f719c844c89
norislav <rino.sincic0@gmail.com>
2023-04-22 12:16:35 +0200

    Aggiornamento fondale
    Invece di usare un modello glb, si crea un mesh in base alla displacement map.
    Al momento si usa usePlane come collider ma e' piatto..

--- prova-react/src/components/Map.jsx
+++ prova-react/src/components/Map.jsx
@@ -1,37 +1,45 @@
-import React, { Suspense } from "react";
+import React from "react";
 import { usePlane } from "@react-three/cannon";
-import { useTexture, useGLTF } from "@react-three/drei";
-import { RepeatWrapping } from "three";
+import { useTexture } from "@react-three/drei";
+import { RepeatWrapping, Color, MeshStandardMaterial } from "three";
 
-import sandTextureURL from "../assets/color.jpg";
-import fondaleModelURL from "../assets/fondale.glb";
-import Test from "./Test.jsx";
+import sandTextureURL from "../assets/map/color.jpg";
+import displacementMapURL from "../assets/map/displacement.png";
+import normalMapURL from "../assets/map/normal.jpg";
+import ambientOcclusionMapURL from "../assets/map/ambientOcclusion.jpg";
+import roughnessMapURL from "../assets/map/roughness.jpg";
+import Test from "./Test";
 
 const Map = () => {
-  // Load sand texture
+  // Load textures
   const sandTexture = useTexture(sandTextureURL, undefined, (texture) => {
     texture.wrapS = texture.wrapT = RepeatWrapping;
     texture.repeat.set(20, 20);
   });
+  const displacementMap = useTexture(displacementMapURL);
+  const normalMap = useTexture(normalMapURL);
+  const ambientOcclusionMap = useTexture(ambientOcclusionMapURL);
+  const roughnessMap = useTexture(roughnessMapURL);
 
-  // Load Fondale model
-  const { scene } = useGLTF(fondaleModelURL);
-
-  console.log("Texture loaded:", sandTexture);
-  console.log("Scene:", scene);
-
-  // Apply texture to the model's mesh
-  scene.traverse((child) => {
-    if (child.isMesh) {
-      child.material.map = sandTexture;
-      child.material.needsUpdate = true;
-    }
+  // Material
+  const material = new MeshStandardMaterial({
+    map: sandTexture,
+    displacementMap: displacementMap,
+    displacementScale: 1,
+    normalMap: normalMap,
+    aoMap: ambientOcclusionMap,
+    roughnessMap: roughnessMap,
+    color: new Color(0xffedab), // Adjust color to be more yellowish like sand
   });
+
+  // Ground size
+  const groundSize = 50;
 
   // Plane collider
   const [ref] = usePlane(() => ({
+    args: [groundSize, groundSize],
     rotation: [-Math.PI / 2, 0, 0],
-    position: [0, -0.25, 0],
+    position: [0, -0.5, 0], // Match the position of the terrain
     material: {
       friction: 0.1,
     },
@@ -39,8 +47,10 @@ const Map = () => {
 
   return (
     <>
-      {/* Fondale model */}
-      <primitive object={scene} />
+      {/* Ground */}
+      <mesh ref={ref} material={material} >
+        <planeBufferGeometry args={[groundSize, groundSize, 100, 100]} />
+      </mesh>
       <Test />
     </>
   );
