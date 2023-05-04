c7db72ebfa26b5f9ee56ad45e7ae4cc451af30aa
norislav <rino.sincic0@gmail.com>
2023-05-04 10:48:16 +0200

    Carrello

--- prova-react/src/features/UI/UI.jsx
+++ prova-react/src/features/UI/UI.jsx
@@ -1,9 +1,13 @@
+import { useContext } from "react";
 import PlayerPosition from "./UI_elements/PlayerPosition";
 import Crosshair from './UI_elements/Crosshair';
 import ProductUI from "./Product_UI/ProductUI";
 import Cart from "../cart/Cart";
 
+import SidebarContext from "../../common/SidebarContext";
+
 const UI = ({ controlsRef }) => {
+    const { isSidebarVisible, setIsSidebarVisible } = useContext(SidebarContext);
 
     return(
         <>  
@@ -11,7 +15,7 @@ const UI = ({ controlsRef }) => {
             <Crosshair />
             <PlayerPosition />
             <ProductUI controlsRef={controlsRef} />
-            <Cart />
+            {isSidebarVisible && <Cart />}
         </div>
 
         </>
