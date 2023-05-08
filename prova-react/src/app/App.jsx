a795770284b82c6fa9fb564017ec2bba4e92920d
Marco Brigo <113689956+MarkBridge11@users.noreply.github.com>
2023-05-08 17:07:49 +0200

    merge from  fix_cart into dev (#215)
    * fix carrello e stilistiche
    
    * code sistemato css carrello e caricamento interfaccia dopo la scena
    
    * minor fixes
    
    ---------
    
    Co-authored-by: MarkBridge11 <marco.brigo@studenti.unipd.it>
    Co-authored-by: mirkostella <mirko.stella@studenti.unipd.it>

--- prova-react/src/app/App.jsx
+++ prova-react/src/app/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, {useState} from "react";
 import { Canvas } from "@react-three/fiber";
 import Scene from "../features/scene/Scene";
 import UI from "../features/UI/UI";
@@ -8,15 +8,15 @@ import SidebarContext from "../common/SidebarContext";
 export default function App() {
   const [intersectedProductID, setIntersectedProductID] = useState(null);
   const [isSidebarVisible, setIsSidebarVisible] = useState(false);
-
+  const [isSceneLoaded,setSceneIsLoad]=useState(false);
   return (
     <>
       <ProductInteractionContext.Provider value={{ intersectedProductID, setIntersectedProductID }}>
         <SidebarContext.Provider value={{ isSidebarVisible, setIsSidebarVisible }}>
-          <UI />     
+          {isSceneLoaded && <UI/>}     
           <Canvas shadows>
             <fog attach="fog" color="black" near={3} far={30} />
-            <Scene />
+            <Scene sceneIsLoad={setSceneIsLoad}/>
           </Canvas>
         </SidebarContext.Provider>
       </ProductInteractionContext.Provider>
