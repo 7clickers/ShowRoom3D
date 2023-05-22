7b13b7b9faf1b146e6d7430a0b5f1ca4f5a8d667
mirkostella <mirko.stella@studenti.unipd.it>
2023-05-22 14:09:06 +0200

    eliminata cartella prova-react

--- prova-react/node_modules/@pmndrs/cannon-worker-api/README.md
+++
@@ -1,3 +0,0 @@
-# @pmndrs/cannon-worker-api
-
-Web worker API for [cannon-es](https://npmjs.com/package/cannon-es). Primarily used in [@react-three/cannon](https://npmjs.com/package/@react-three/cannon).
--- prova-react/prova.test.js
+++
@@ -1,9 +0,0 @@
-import {sum} from './prova'
-import {diff} from './prova'
-import {fun} from './prova'
-
-describe('test fun di prova',()=>{
-    test('primo test della funzione di prova',()=>{
-        
-    })
-})
